119e7e4c98fb523361994eec807ead7fd43f59cb
Jan Georg Henriksen <jangeorg85@gmail.com>
2022-12-05 23:56:49 +0100

    changed some styles, looks ok on mobile now

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,19 +4,19 @@ const Navigation = ({onRouteChange, isSignedIn, route}) => {
   if (isSignedIn) {
     return (
       <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-        <p onClick={()=>onRouteChange('signout')} className="f3 light-gray link dim black underline pa3 pointer white-90">Logg ut</p>
+        <p onClick={()=>onRouteChange('signout')} className="f3 mt-0 light-gray link dim black underline pa2 pointer white-90">Logg ut</p>
       </nav>
     ); 
   } else if (!isSignedIn && (route === 'signin' || route === 'signout')) {
     return (
       <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-          <p onClick={()=>onRouteChange('register')} className="f3 light-gray link dim black underline ph3 pointer white-90">Registrer</p>
+          <p onClick={()=>onRouteChange('register')} className="f3 mt-0 light-gray link dim black underline ph2 pointer white-90">Registrer</p>
         </nav>
     );
   } else {
     return (
       <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-          <p onClick={()=>onRouteChange('signin')} className="f3 light-gray link dim black underline ph3 pointer white-90">Logg inn</p>
+          <p onClick={()=>onRouteChange('signin')} className="f3 mt-0 light-gray link dim black underline ph2 pointer white-90">Logg inn</p>
         </nav>
     );
   }
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -5,7 +5,7 @@ const Rank = ({name, entries}) => {
   return (
     <div>
       <div className="white f3 ma2 mt6">
-        <p>{`Hei ${name}, så mange bilder har du prøvd:`}</p>
+        <p className="mt0 w-70 center">{`Hei ${name}, så mange bilder har du prøvd:`}</p>
       </div>
       <div className="white f1">
         {entries}
